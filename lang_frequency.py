f2d20dc716d10082fc7a651c15cad0ff844c17c5
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-02-20 18:30:28 +0300

    Refactor parser and string formatting.

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,11 +2,13 @@ import re
 import argparse
 from collections import Counter
 
-parser = argparse.ArgumentParser(description='Frequency Analysis of Words')
-parser.add_argument('path', metavar='DIR', help='path to the text file')
-parser.add_argument('--num', type=int, default=10,
-                    help='number of most common words to display')
-args = parser.parse_args()
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Frequency Analysis of Words')
+    parser.add_argument('path', metavar='DIR', help='path to the text file')
+    parser.add_argument('--num', type=int, default=10,
+                        help='number of most common words to display')
+    return parser
 
 
 def load_data(filepath):
@@ -28,11 +30,14 @@ def get_most_frequent_words_and_their_frequency(text, num=10):
 
 
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
     text = load_data(args.path)
     words_and_freq = get_most_frequent_words_and_their_frequency(text, args.num)
 
     print('Most common words:')
-    print('\n'.join('%s: %d' % (t[0], t[1]) for t in words_and_freq))
+    print('\n'.join('{}: {}'.format(t[0], t[1]) for t in words_and_freq))
 
 
 if __name__ == '__main__':
